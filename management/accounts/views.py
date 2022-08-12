bc5d2af987fdb5cb7b33f64ad044dbb8336e2bce
Jerry Wang <jerry.wang2352@gmail.com>
2022-08-12 16:53:03 -0400

    Render order data into dashboard and products page

--- management/accounts/views.py
+++ management/accounts/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-#from django.http import HttpResponse 
+from django.http import HttpResponse 
+from .models import * 
 
 # Create your views here.
 
 def home(request):
-    return render(request,'dashboard.html')
+    orders = Order.objects.all() 
+    customers = Customer.objects.all() 
+
+    total_customers = customers.count() 
+    total_orders = orders.count() 
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+    
+    return render(request,'dashboard.html',{"orders":orders,"customers":customers,"total_orders":total_orders,"delivered":delivered,"total_customers":total_customers,"pending":pending})
+
+
 def products(request):
-    return render(request,'products.html')
+    products = Product.objects.all() 
+    return render(request,'products.html', {'products':products})
+
+
 def customer(request):
     return render(request,'customer.html')
